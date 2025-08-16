bb3ad03d40df8c6b2fa896d5c9b5ce0f674991de
kellytan <kellytxn@gmail.com>
2025-08-16 15:53:10 +0800

    finished level 0

---
+++ src/main/java/Haru.java
@@ -0,0 +1,17 @@
+public class Haru {
+    public static void main(String[] args) {
+        String logo = "   ___ ___                     \n" +
+                " /   |   \\_____ _______ __ __ \n" +
+                "/    ~    \\__  \\\\_  __ \\  |  \\\n" +
+                "\\    Y    // __ \\|  | \\/  |  /\n" +
+                " \\___|_  /(____  /__|  |____/ \n" +
+                "       \\/      \\/             ";
+
+        System.out.println("--------------------------------------");
+        System.out.println("Hello! I'm \n" + logo);
+        System.out.println("What can I do for you today? \n");
+        System.out.println("--------------------------------------");
+        System.out.println("Bye. Hope to see you again soon! \n");
+        System.out.println("--------------------------------------");
+    }
+}
