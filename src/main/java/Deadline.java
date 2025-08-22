fef44b68bbb9dc1809d9519cea194cdca9d88ae9
kellytan <kellytxn@gmail.com>
2025-08-22 16:30:14 +0800

    Change date format for deadline tasks

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    protected String end;
+    protected LocalDate end;
+
+    private static final DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MMM dd yyyy");
 
     public Deadline(String name, String end, Type type) {
         super(name, type);
-        this.end = end;
+        this.end = LocalDate.parse(end, inputFormat);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.end + ")";
+        return "[D]" + super.toString() + " (by: " + this.end.format(outputFormat) + ")";
     }
 
     @Override
     public String toFileString() {
+
         return "D | " + super.toFileString() + " | " + this.end;
     }
 }
